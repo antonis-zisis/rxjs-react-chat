728e42116893a9861a4d7f09457a4250653985df
Antonis <antonis.zisis.dev@gmail.com>
2025-04-13 22:50:02 +0300

    ui improvements

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { sendMessage, chatStream } from '../chat';
 
 export function Chat({ username }) {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
+
+  const chatContainerRef = useRef(null);
 
   useEffect(() => {
     const sub = chatStream.subscribe((msg) => {
@@ -13,6 +15,15 @@ export function Chat({ username }) {
 
     return () => sub.unsubscribe();
   }, []);
+
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTo({
+        top: chatContainerRef.current.scrollHeight,
+        behavior: 'smooth',
+      });
+    }
+  }, [messages]);
 
   const handleSend = () => {
     if (input.trim()) {
@@ -31,31 +42,45 @@ export function Chat({ username }) {
           the chat!
         </h1>
 
-        <div className="h-64 overflow-y-auto rounded bg-white p-4 shadow-md">
+        <div
+          className="h-64 overflow-y-auto rounded bg-white p-4 shadow-md"
+          ref={chatContainerRef}
+        >
           <div className="flex flex-1 flex-col gap-2 overflow-y-auto p-4">
-            {messages.map((msg, index) => {
-              const isMine = msg.sender === username;
+            {messages.map((message, index) => {
+              const isMine = message.sender === username;
+              const timestamp = new Date(message.timestamp).toLocaleTimeString(
+                [],
+                {
+                  hour: '2-digit',
+                  minute: '2-digit',
+                }
+              );
 
               return (
                 <div
                   key={index}
                   className={`flex ${isMine ? 'justify-end' : 'justify-start'}`}
                 >
-                  <div className="flex flex-col">
-                    {!isMine && (
-                      <div className="mb-0.5 ml-2 text-xs font-normal text-slate-400">
-                        {msg.sender}
-                      </div>
-                    )}
+                  <div
+                    className={`flex max-w-full flex-col ${isMine ? 'items-end' : 'items-start'}`}
+                  >
+                    <div
+                      className={`mb-0.5 text-xs font-normal text-slate-400 ${
+                        isMine ? 'pr-2 text-right' : 'self-start pl-2 text-left'
+                      }`}
+                    >
+                      {isMine ? timestamp : `${message.sender} - ${timestamp}`}
+                    </div>
 
                     <div
-                      className={`max-w-xs rounded-2xl px-4 py-2 shadow md:max-w-sm ${
+                      className={`w-fit max-w-xs rounded-2xl px-4 py-2 shadow md:max-w-sm ${
                         isMine
                           ? 'rounded-br-none bg-teal-500 text-white'
                           : 'rounded-bl-none bg-slate-200 text-slate-800 dark:bg-slate-700 dark:text-white'
                       }`}
                     >
-                      <div>{msg.text}</div>
+                      <div>{message.text}</div>
                     </div>
                   </div>
                 </div>
